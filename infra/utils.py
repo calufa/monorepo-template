73ef564caf37769b4848e989e17213b1601ec378
Carlos Chinchilla <calufa@gmail.com>
2021-08-30 23:16:54 -0600

    adjust utils.py

--- infra/utils.py
+++ infra/utils.py
@@ -1,10 +1,20 @@
 import glob
 import os
 
+import yaml
+
+config_file = "infra.yaml"
+
+
+def get_app_config(app_path):
+    with open(f"{app_path}/{config_file}") as f:
+        config = yaml.safe_load(f)
+    return config
+
 
 def get_app_paths():
     paths = []
     for path in glob.glob("/app/*"):
-        if path != os.getcwd() and os.path.exists(f"{path}/infra.yaml"):
+        if os.path.exists(f"{path}/{config_file}"):
             paths.append(path)
     return paths
